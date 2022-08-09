06c62979a6137f77583c1a673fd0e8b351ef5e87
Gustavo <unidragorn@gmail.com>
2022-08-09 16:48:31 -0300

    adiciona splitSentence

--- src/challenges.js
+++ src/challenges.js
@@ -12,8 +12,8 @@ function calcArea(base, altura) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(divideString) {
+  return divideString.split(' ');
 }
 
 // Desafio 4
